3893f57b39f80030ef3cc9d81775043115f8d10a
Leonardo Souza <leonardostsouza@gmail.com>
2018-11-24 15:19:31 -0200

    Implement block_get and block_put on Infura

--- src/main.rs
+++ src/main.rs
@@ -21,50 +21,10 @@ use self::ipfsapi::IpfsApi;
 use std::env::args;
 
 fn main() {
-    // Ethereum Network Test
-    /*let (_eloop, http) = web3::transports::Http::new("https://ropsten.infura.io/").unwrap();
-    let web3 = web3::Web3::new(http);
-    let bln = web3.eth().block_number().wait().unwrap();
-    println!("Latest Block Number: {:?}", bln);
-
-    let acc = web3.eth().accounts().wait().unwrap();
-    println!("Acounts: {:?}", acc);
-
-    let vec: &[u8] = b"0xcEBD3825A1995A30EA8Bd0666E959f0c8bcEfCF3";
-    let mut addr: Address = Address::new();
-    addr.clone_from_slice(vec);
-    //let addr: Address = Address::random();
-    let balance = web3.eth().balance(addr, None).wait().unwrap();
-    println!("Vec: {:?}\nAddress: {:x?}\nBalance: {:?}", vec, addr, balance);*/
-
-    // IPFS test
-    //let url = "https://ipfs.infura.io:5001/api/v0/block/get?arg=";
-    let hash = "QmfM2r8seH2GiRaC4esTjeraXEachRt8ZsSeGaWTPLyMoG";
-    //let body = reqwest::get([url, hash].join("").as_str());
-    //println!("Body:\n{:?}", body.unwrap().text());
-
     // IPFS Mod test
+    let hash = "Qmb3qWYf462ypcjP9KRLAozEM7xSo4aDNNJchFZfDSyQSE";
     println!("{:?}", ipfs::block_get(hash));
-    //ipfs::block_get(hash);
-
-
-
-    println!("1");
-    let api = IpfsApi::new("ipfs.infura.io", 5001);
-    //let hash = "QmfM2r8seH2GiRaC4esTjeraXEachRt8ZsSeGaWTPLyMoG";
-    println!("2");
-    let bytes = match api.block_get(&hash){
-            Ok(raw_data) => raw_data,
-            Err(error) => {
-                return
-            }
-    };
-    println!("3");
-    let data =  String::from_utf8(bytes.collect())
-                .expect("Unable read data from IPFS block as string");
-    println!("4");
-    println!("{}", data);
-    println!("5");
+    
 
 
     /*let application = gtk::Application::new("org.ethereum.rs.moon",
